959462abf93fd77f1d635719120a1a403441b597
Colindustrys <mail@colindustrys.com>
2023-09-08 13:54:21 +0200

    pwa setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
-export const metadata = {
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
   title: 'Schlagball',
+  manifest: "/manifest.json",
+  icons: { apple: "/icon.png" },
+  themeColor: "#fff",
 }
 
 export default function RootLayout({
