dfcf8a53e268c6d37bdf721ebe5ac305909b1681
Colindustrys <mail@colindustrys.com>
2023-09-20 14:47:01 +0200

    Revert "menu und spiel seite getauscht"
    This reverts commit 1cbda59484dc7d51b10acaf3f5ffc2b935c25707.

---
+++ app/menu/page.tsx
@@ -0,0 +1,131 @@
+"use client"
+import React, { useState, useEffect, FormEvent } from "react";
+import Cookies from "js-cookie";
+import GameData from "@/types/gameData";
+
+interface FormData {
+  team1name: string;
+  team2name: string;
+  timer: number;
+}
+
+interface FormProps {
+  onSubmit: (data: FormData) => void;
+}
+
+export default function Menu() {
+
+  const [formData, setFormData] = useState<FormData>({
+    team1name: '',
+    team2name: '',
+    timer: 0,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    
+    const { name, value } = e.target;
+
+    console.log(name + " " + value);
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  function onSumbitHandler(e: React.FormEvent) {
+    e.preventDefault();
+    
+    newGame(formData.team1name, formData.team2name, formData.timer)
+    
+  }
+
+  function newGame(team1name: string, team2name: string, timer: number) {
+  
+    //make new game object 
+    let newGame: GameData = {
+      team1name: team1name,
+      team1points: 0,
+      team1currentPlayer: 0,
+
+      team2name: team2name,
+      team2points: 0, 
+      team2currentPlayer: 0,
+
+      currentTeam: 0,
+
+      setGameLength: timer,
+      timestamp: null,
+      events: []
+    }
+
+    //save cookie 
+    //Cookies.set('myJsonData', JSON.stringify(newGame), { sameSite: 'lax', expires: 365 });
+    localStorage.setItem("myGameData", JSON.stringify(newGame))
+    
+
+    //save empty events 
+    //Cookies.set('myEventData', JSON.stringify([]), { sameSite: 'lax', expires: 365 });
+    localStorage.setItem("myEventsData", JSON.stringify([]))
+
+    //direct to game page
+    window.location.href = "/";
+    
+  }
+
+  function loadGame() {
+    //link to game page 
+    window.location.href = "/";
+  }
+  
+
+  return (
+    <div>
+      <header>
+        <h1>Menu</h1>
+      </header>
+      
+      <main>
+        <div>
+          <button onClick={loadGame}>Load Game</button>
+        </div>
+        <div>
+          <form onSubmit={onSumbitHandler}>
+            <div>
+              <label htmlFor="input1">Team 1 Name:</label>
+              <input
+                type="text"
+                id="input1" 
+                name="team1name"
+                value={formData.team1name}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="input2">Team 2 Name</label>
+              <input
+                type="text"
+                id="input2"
+                name="team2name"
+                value={formData.team2name}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="input3">Timer länge</label>
+              <input
+                type="number"
+                id="input3"
+                name="timer"
+                value={formData.timer}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">New Game</button>
+          </form>
+          
+        </div>
+      </main>
+    </div>
+
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,129 +1,225 @@
 "use client"
-import React, { useState, useEffect, FormEvent } from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
+import TestComponent from "../components/testComponent";
+import CurrentPlayer from "@/components/currentPlayer";
 import GameData from "@/types/gameData";
+import EventT from "@/types/eventT";
+import TimeLeft from "@/components/timeLeft";
+import ScoreBoard from "@/components/score";
+import TeamNameDisplay from "@/components/teamNameDisplay";
+import PlayerSelector from "@/components/playerSelector";
+import PointButtons from "@/components/pointButtons";
 
-interface FormData {
-  team1name: string;
-  team2name: string;
-  timer: number;
+import 'reactjs-popup/dist/index.css';
+import EventLog from "@/components/eventLog"
+import TimerStartButton from "@/components/timerStartButton"
+
+let myJson: GameData = {
+  "team1name": "Eagles",
+  "team1points": 30, 
+  "team2name": "lakers",
+  "team2points": 20,
+  "currentTeam": 0,
+  "team1currentPlayer": 0,
+  "team2currentPlayer": 0,
+  "timestamp": null,
+  "setGameLength": 3,
+  "events": []
 }
 
-interface FormProps {
-  onSubmit: (data: FormData) => void;
-}
+let myEvents: EventT[] = []
 
-export default function Menu() {
+export default function Home() {
 
-  const [formData, setFormData] = useState<FormData>({
-    team1name: '',
-    team2name: '',
-    timer: 0,
-  });
+  useEffect(() => {
+    loadData()
+  }, []);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    
-    const { name, value } = e.target;
+  const [json, setJson] = useState(myJson);
+  const [events, setEvents] = useState(myEvents)
 
-    console.log(name + " " + value);
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  useEffect(() => {
+    saveData()
+  }, [json, events]);
 
-  function onSumbitHandler(e: React.FormEvent) {
-    e.preventDefault();
-    
-    newGame(formData.team1name, formData.team2name, formData.timer)
-    
+  function pointTeam1() {
+    let newJson = JSON.parse(JSON.stringify(json));
+    newJson.team1points++;
+    setJson(newJson)  
+    return newJson  
   }
 
-  function newGame(team1name: string, team2name: string, timer: number) {
-  
-    //make new game object 
-    let newGame: GameData = {
-      team1name: team1name,
-      team1points: 0,
-      team1currentPlayer: 0,
+  function pointTeam2() {
+    let newJson = JSON.parse(JSON.stringify(json));
+    newJson.team2points++;
+    setJson(newJson)
+    return newJson
+  }
 
-      team2name: team2name,
-      team2points: 0, 
-      team2currentPlayer: 0,
-
-      currentTeam: 0,
-
-      setGameLength: timer,
-      timestamp: null,
-      events: []
+  function loadData() {
+    let storedData = localStorage.getItem("myGameData");
+    if (storedData) {
+      setJson(JSON.parse(storedData));
     }
 
-    //save cookie 
-    //Cookies.set('myJsonData', JSON.stringify(newGame), { sameSite: 'lax', expires: 365 });
-    localStorage.setItem("myGameData", JSON.stringify(newGame))
+    storedData = localStorage.getItem("myEventsData");
+    if (storedData) {
+      setEvents(JSON.parse(storedData));
+    }
+
+  }
+
+  function saveData() {
+    // Cookies.set('myGameData', JSON.stringify(json), { sameSite: 'lax', expires: 365 });
+    // Cookies.set('myEventData', JSON.stringify(events), { sameSite: 'lax', expires: 365 });
+
+    localStorage.setItem("myGameData", JSON.stringify(json))
+    localStorage.setItem("myEventsData", JSON.stringify(events))
+  }
+
+  function nextPlayer() {
+    let newJson: GameData = JSON.parse(JSON.stringify(json));
+
+    //check current team
+    if (newJson.currentTeam == 0) {
+      //team 1
+      newJson.team1currentPlayer++;
+      newJson.team1currentPlayer = newJson.team1currentPlayer % 12;
+    } else {
+      //team 2
+      newJson.team2currentPlayer++;
+      newJson.team2currentPlayer = newJson.team2currentPlayer % 12;
+    }
+
+    setJson(newJson)
+  }
+
+  function toterWechsel() {
+    switchTeam(JSON.parse(JSON.stringify(json)))
+  }
+
+  function switchTeam(newJson: GameData) {
+    //let newJson: GameData = JSON.parse(JSON.stringify(json));
+    newJson.currentTeam++;
+    newJson.currentTeam = newJson.currentTeam % 2
+
+    setJson(newJson)
+  }
+
+  function addLog(team: number, player: number, text: string) {
+    let newEvents: EventT[] = JSON.parse(JSON.stringify(events));
+    newEvents.push({
+      "timestampt": Date.now(),
+      "team": team,
+      "player": player,
+      "text": text
+    })
     
-
-    //save empty events 
-    //Cookies.set('myEventData', JSON.stringify([]), { sameSite: 'lax', expires: 365 });
-    localStorage.setItem("myEventsData", JSON.stringify([]))
-
-    //direct to game page
-    window.location.href = "/game";
+    setEvents(newEvents)
+    
     
   }
 
-  function loadGame() {
-    //link to game page 
-    window.location.href = "/game";
+  function point(team: number, player: number, art: string) {
+    //add event 
+    console.log(team + " " + player + " " + art);
+    addLog(team, player, art)
+
+    console.log(team + " " + player + " " + art);
+    
+    
+    
+    let newJson: GameData
+
+    if (team == 1) {
+      console.log(team);
+      
+      newJson = pointTeam1()
+    } else {
+      console.log(team);
+      
+      newJson = pointTeam2()
+    }
+    //console.log(json);
+    
+    //hier ist ein problem mit dem usestate da in switch team der usestate json genutzt wird bevor das update von pointTeam() da ist im usestate 
+    if (art == "Abwurfpunkt") {
+      switchTeam(newJson);
+    }
   }
-  
+
+  function temp() {
+    //Cookies.remove('')
+    //window.location.href = "/menu";
+
+    // let events: EventT[] = [
+    //   {"timestampt": 0, "text": "5etgrkdfsm", "team": 3, "player": 3},
+    //   {"timestampt": 0, "text": "5etgrkdfsm", "team": 3, "player": 3},
+    // ]
+    // console.log(JSON.stringify(events));  
+
+    localStorage.setItem("myEventsData", JSON.stringify(events))
+    localStorage.removeItem('test');
+
+    // Remove data from localStorage
+    //localStorage.removeItem('userData');
+
+    
+
+    
+  }
+
+  function endGame() {
+    alert("Game Over!");
+  }
+
+  function clearCookies() {
+    // Cookies.remove("myJsonData")
+    // Cookies.remove("myEventData")
+
+    localStorage.removeItem('myGameData');
+    localStorage.removeItem('myEventsData');
+    window.location.href = "/menu";
+  }
+
+  function timerStart() {
+    if (json.timestamp === null) {
+      json.timestamp = Date.now()
+    }
+    saveData()
+  }
 
   return (
     <div>
       <header>
-        <h1>Menu</h1>
+        <h1 className="text-3xl font-bold underline">My Page</h1>
       </header>
       
       <main>
-        <div>
-          <button onClick={loadGame}>Load Game</button>
-        </div>
-        <div>
-          <form onSubmit={onSumbitHandler}>
-            <div>
-              <label htmlFor="input1">Team 1 Name:</label>
-              <input
-                type="text"
-                id="input1" 
-                name="team1name"
-                value={formData.team1name}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="input2">Team 2 Name</label>
-              <input
-                type="text"
-                id="input2"
-                name="team2name"
-                value={formData.team2name}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="input3">Timer länge</label>
-              <input
-                type="number"
-                id="input3"
-                name="timer"
-                value={formData.timer}
-                onChange={handleChange}
-              />
-            </div>
-            <button type="submit">New Game</button>
-          </form>
-          
-        </div>
+        
+        <TeamNameDisplay json={json} teamNR={1}/>
+        <ScoreBoard json={json}/>
+        <TeamNameDisplay json={json} teamNR={2}/>
+
+        <button onClick={temp}>temp</button>
+
+        <TestComponent loadData={loadData} saveData={saveData}/>
+        <CurrentPlayer json={json} nextPlayer={nextPlayer}/>
+        <TimeLeft json={json} endGameCallback={endGame}/>
+        <button onClick={toterWechsel}>Toter Wechsel</button>
+        
+        <p>Punkte Team 1</p>
+
+        <PointButtons pointFunc={point} team={1}/>
+
+        <p>Punkte Team 2</p>
+
+        <PointButtons pointFunc={point} team={2}/>
+        <a href="/menu">menu</a>
+        <button onClick={clearCookies}>DeleteGame</button>
+        <EventLog events={events}/>
+        <TimerStartButton json={json} onClickFunction={timerStart}/>
       </main>
     </div>
 
